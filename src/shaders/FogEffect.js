0903294363edb640f221a3fd880c4466aca4417a
Tom Hinton <tomjhinton@gmail.com>
2022-12-18 17:10:02 +0000

    test

--- src/shaders/FogEffect.js
+++ src/shaders/FogEffect.js
@@ -16,22 +16,47 @@ void coswarp(inout vec3 trip, float warpsScale ){
     
   }  
 
+  void uvRipple(inout vec2 uv, float intensity){
+
+	vec2 p = uv -.5;
+
+
+    float cLength=length(p);
+
+     uv= uv +(p/cLength)*cos(cLength*15.0-offset*.5)*intensity;
+
+} 
+
 void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
 {
     vec4 color = inputColor;
     float t = (offset * .2) + length(uv-.5);
 
-    vec2 uv2 = fract(uv * 10.);
+    vec2 uv2 = uv;
     float alpha =   .5;
 
-    vec3 color2 = vec3(uv.x, uv.y, 1.);
+    uvRipple(color.rg, 2.4);
+    coswarp(color.rgb, 3.);
+    coswarp(color.rgb, 3.);
+    coswarp(color.rgb, 3.);
+
+    vec3 color2 = vec3(uv2.x, uv2.y, 1.);
     coswarp(color2, 3.);
     coswarp(color2, 3.);
     coswarp(color2, 3.);
 
     alpha+= (color2.r *.25);
     
-   vec3 color3 = mix(inputColor.rgb, color2, sin(t));
+    
+   vec3 color3 = mix(color.rgb, color2, 1.);
+
+   if(uv.x < .5){
+       color3 = inputColor.rgb;
+   }
+
+   if(uv.x < .35){
+    color3 = vec3(step(color.r, .5 ), step(color.r, .4), step(color.r, .3));
+}
     
     outputColor = vec4(color3, 1.);
     //outputColor = vec4(.8, 1., .3, inputColor.a);
